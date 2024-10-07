993d82c64553bd0dcf4f5e02c24448f145456f6c
Gramatin Artiom <121014424+GramatinArtiom@users.noreply.github.com>
2024-10-07 19:39:49 +0300

    Initial Commit

---
+++ expenseTracker/expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerApp.swift
+//  expenseTracker
+//
+//  Created by Artiom Gramatin on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expenseTrackerTests/expenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerTests.swift
+//  expenseTrackerTests
+//
+//  Created by Artiom Gramatin on 07.10.2024.
+//
+
+import Testing
+@testable import expenseTracker
+
+struct expenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
